a13b84c7be3319289eb64f9ebee4a9b2c1a8b1ca
XIAOAO S <45730495+lineojcd@users.noreply.github.com>
2024-04-25 00:43:07 -0700

    Add files via upload

---
+++ scripts/closed_loop.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from math import pi, sqrt, atan2, cos, sin
+import numpy as np
+
+import rospy
+import tf
+from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Pose2D
+
+class Turtlebot():
+    def __init__(self):
+        rospy.init_node("turtlebot_move")
+        rospy.loginfo("Press Ctrl + C to terminate")
+        self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.rate = rospy.Rate(10)
+
+        # reset odometry to zero
+        self.reset_pub = rospy.Publisher("mobile_base/commands/reset_odometry", Empty, queue_size=10)
+        for i in range(10):
+            self.reset_pub.publish(Empty())
+            self.rate.sleep()
+        
+        # subscribe to odometry
+        self.pose = Pose2D()
+        self.logging_counter = 0
+        self.trajectory = list()
+        self.odom_sub = rospy.Subscriber("odom", Odometry, self.odom_callback)
+
+        try:
+            self.run()
+        except rospy.ROSInterruptException:
+            rospy.loginfo("Action terminated.")
+        finally:
+            # save trajectory into csv file
+            np.savetxt('trajectory.csv', np.array(self.trajectory), fmt='%f', delimiter=',')
+
+
+    def run(self):
+        # add your code here to adjust your movement based on 2D pose feedback
+        # Use the Controller
+        pass
+
+
+    def odom_callback(self, msg):
+        # get pose = (x, y, theta) from odometry topic
+        quarternion = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,\
+                    msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quarternion)
+        self.pose.theta = yaw
+        self.pose.x = msg.pose.pose.position.x
+        self.pose.y = msg.pose.pose.position.y
+
+        # logging once every 100 times (Gazebo runs at 1000Hz; we save it at 10Hz)
+        self.logging_counter += 1
+        if self.logging_counter == 100:
+            self.logging_counter = 0
+            self.trajectory.append([self.pose.x, self.pose.y])  # save trajectory
+            # display (x, y, theta) on the terminal
+            rospy.loginfo("odom: x=" + str(self.pose.x) +\
+                ";  y=" + str(self.pose.y) + ";  theta=" + str(yaw))
+
+
+if __name__ == '__main__':
+    whatever = Turtlebot()
